211f1a15823e26f4b2960114ff6db0d813c7d8ef
jezp27 <jmzapatap@unal.edu.co>
2019-06-12 22:48:05 -0500

    Styles

--- global.R
+++ global.R
@@ -1,3 +1,5 @@
+library(raster)
+
 comunas <- c("America",
              "Aranjuez",
              "Belen",
--- ui.R
+++ ui.R
@@ -1,40 +1,64 @@
 library(shiny)
 library(leaflet)
 library(ggplot2)
+library(shinyBS)
+library(shinythemes)
+library(shinyLP)
 
 dataset <- diamonds
 
-shinyUI(fluidPage(
+shinyUI(
+  navbarPage("********************",
+      #theme = "style.css",
+      theme = shinytheme("flatly"),
+      #shinythemes::themeSelector(),
+      tabPanel(
+        "Inicio",
+        
+        jumbotron("Accidentalidad en Medellin", "Historial y Prediccion", button = FALSE),
+        
+        sidebarLayout(
+          sidebarPanel(
+                          
+          ),
+          mainPanel(
+                          
+          )
+        )
+      ),
+      tabPanel(
+        "Historial",
+        #panel_div(class_type = "primary", panel_title = "Criterios",
+         #         content = 
+         sidebarPanel(
+            selectInput('_Comuna', 'Seleccione una comuna:', comunas),
+            selectInput('_Barrio', 'Seleccione un barrio:', iconv(barrios@data$NOMBRE,"UTF-8","ISO_8859-1")),
+            sliderInput('_Year', 'Fecha', min=2014, max=2018, value=min(2014, 2018), step=1, round=0),
+            #selectInput('x', 'X', names(dataset)),
+            #selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+            selectInput('color', 'Color', c('None', names(dataset))),
+            checkboxInput('jitter', 'Jitter'),
+            checkboxInput('smooth', 'Smooth'),
+                        
+            selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+            selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+         ), #),
+         mainPanel(
+            leafletOutput("myMap")
+            #plotOutput('plot')
+         )
+      )
+    )
+  )
+             
   
-  theme = "bootstrap.css",
+  #fluidPage(
+  
+
 
   #tags$head(rel = "stylesheet", type = "text/css", href = "bootstrap.css"),
-            
-  #titlePanel("Accidentalidad en Medellin"),
-  headerPanel("Accidentalidad en Medellin", "Accidentalidad en Medellin"),
-  #sidebarPanel(),
 
- sidebarPanel(
-   
-   selectInput('_Comuna', 'Seleccione una comuna:', comunas),
-   selectInput('_Barrio', 'Seleccione un barrio:', iconv(barrios@data$NOMBRE,"UTF-8","ISO_8859-1")),
-   
-    sliderInput('_Year', 'Fecha', min=2014, max=2018,
-               value=min(2014, 2018), step=1, round=0),
+ 
+  
 
-    #selectInput('x', 'X', names(dataset)),
-    #selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
 
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-  ),
-
-  mainPanel(
-    leafletOutput("myMap")
-    #plotOutput('plot')
-  )
-))
